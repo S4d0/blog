efb59565a9b3c6e5aee71162dde2043f6173b126
S4d0 <saadettinatay@hotmail.com>
2022-02-06 08:28:30 +0300

    my web site

---
+++ resources/views/home/_header.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>s4d0 my blog</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/animate.css">
+
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/aos.css">
+
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/icomoon.css">
+    <link rel="stylesheet" href="{{ asset('assets/home') }}/css/style.css">
+</head>
+<body>
+
+
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,4 @@
+@include('home._header')
+@include('home._sidebar')
+@include('home._content')
+@include('home._footer')
